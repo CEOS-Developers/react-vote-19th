e421193bda82c3b840fea96f148fe2a2f5c4d0de
Dahn12 <dadesign0709@gmail.com>
2024-06-21 17:46:47 +0900

    fix : import 정리

--- react-vote/src/index.tsx
+++ react-vote/src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import GlobalStyle from "./styles/GlobalStyle"
 import { ThemeProvider } from "styled-components";
-import theme from './styles/Theme';
-
+import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
