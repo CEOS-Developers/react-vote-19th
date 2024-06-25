4b34fba8576ef59780f6cd250836536655675e58
안혜연 <ahy020606@naver.com>
2024-06-25 17:52:41 +0900

    etc: develop merge

--- vote/src/pages/Vote/VoteFront.tsx
+++ vote/src/pages/Vote/VoteFront.tsx
@@ -60,6 +60,7 @@ export default function VoteFront() {
   };
 
   const handleSubmit = () => {
+    console.log("투표 완료", selectedFront);
     if (selectedFront !== null) {
       postVoteMutate({
         topicId: 1,
