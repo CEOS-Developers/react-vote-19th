8eed89864d249e618322cf5f3bbea400ee35006a
Rose <120544840+Rose-my@users.noreply.github.com>
2024-06-25 15:39:21 +0900

    feat: postSignUp 파라미터 추가

--- vote/src/api/postSignUp.ts
+++ vote/src/api/postSignUp.ts
@@ -2,13 +2,14 @@ import { postSignUpTypes } from "types/postSignUpTypes";
 import { customAxios } from "./customAxios";
 
 export async function postSignUp(props: postSignUpTypes) {
-  const { username, password, email, part, team, name } = props;
+  const { username, password, email, part, team, name, role } = props;
   const response = await customAxios.post("/api/users/signup", {
     username: username,
     password: password,
     email: email,
     part: part,
     team: team,
+    role: role,
     name: name,
   });
 
