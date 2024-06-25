1353361385433326711fbc942dab2bf5aeccc309
Rose <120544840+Rose-my@users.noreply.github.com>
2024-06-25 15:41:19 +0900

    feat: hooks/usePostEmail

--- vote/src/hooks/usePostEmail.ts
+++ vote/src/hooks/usePostEmail.ts
@@ -3,8 +3,8 @@ import { postEmail } from "@api/postEmail";
 
 export function usePostEmail() {
   return useMutation(postEmail, {
-    onSuccess: () => {
-      console.log("이메일 인증 성공");
+    onSuccess: (data) => {
+      console.log("이메일 인증 성공", data);
     },
     onError: (error) => {
       console.log("이메일 인증 실패", error);
