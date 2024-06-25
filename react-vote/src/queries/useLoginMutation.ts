14398eced3f90b7a9c97135561197d146c82c7f7
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-06-25 19:19:03 +0900

    fix : refreshtoken cookie -> header

--- react-vote/src/queries/useLoginMutation.ts
+++ react-vote/src/queries/useLoginMutation.ts
@@ -2,12 +2,9 @@ import { useMutation } from '@tanstack/react-query';
 import React from 'react';
 import { authAPI, LoginData } from '../api/request';
 import { useNavigate } from 'react-router-dom';
-import { getCookie } from '../utils/cookie';
 
 export const useLoginMutation = () => {
 	const navigate = useNavigate();
-	// let refreshToken = getCookie('refreshToken');
-	//const [cookies] = useCookies(['refreshToken']);
 
 	const QUERY_KEY_LOGIN = 'login';
 
@@ -23,17 +20,9 @@ export const useLoginMutation = () => {
 				const accessToken = res.headers.authorization;
 				localStorage.setItem('accessToken', accessToken);
 
-				// 쿠키가 생성될 시간을 대기한 후 쿠키에서 refreshToken 가져오기
-				setTimeout(() => {
-					const refreshToken = getCookie('refreshToken');
-					console.log('refreshToken:', refreshToken); // 확인용 로그
-					if (refreshToken) {
-						localStorage.setItem('refreshToken', refreshToken);
-					} else {
-						console.error('Failed to retrieve refreshToken from cookies');
-					}
-					navigate(`/`);
-				}, 1000); // 1초 대기 후 쿠키 읽기
+				const refreshToken = res.headers.refreshtoken;
+				localStorage.setItem('refreshToken', refreshToken);
+
 			} else if (res.status === 400) {
 				alert('잘못된 아이디/비밀번호입니다.');
 			}
