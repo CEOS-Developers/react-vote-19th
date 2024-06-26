15e4653687c0e2f2e85ca2c983e6e4a4ffe1d267
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-06-26 18:57:20 +0900

    fix : data.value 접근 에러 수정

--- react-vote/src/queries/useGetLeader.ts
+++ react-vote/src/queries/useGetLeader.ts
@@ -2,23 +2,23 @@ import { useQuery } from '@tanstack/react-query';
 import { userAPI } from '../api/request';
 import { useNavigate } from 'react-router-dom';
 
-// 리더를 가져오는 훅
 export const useGetLeaders = (partName: string) => {
 	const navigate = useNavigate();
+
 	const { data, isLoading, error } = useQuery({
-		queryKey: [partName, 'Leaders'],
+		queryKey: ['leaders', partName],
 		queryFn: async () => {
 			const res = await userAPI.getLeaders(partName);
 			if (res.status === 400) {
 				navigate('/login');
 				alert('접근오류! 로그인하세요');
 			}
-			return res.data;
+			return res.data; // res.data의 구조에 맞게 반환
 		},
 	});
 
 	return {
-		leaders: data.value || null,
+		leaders: data?.value || [],
 		isLoading,
 		error,
 	};
